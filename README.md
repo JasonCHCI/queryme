efcd6f6eaaf6d35d3cf793da3332e25cb3a92ab4
wenhua11 <linwenhua560@gmail.com>
2017-10-20 02:14:00 -0500

    initial set up

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Query Me
+This project is a sql query program through command line
+
+##Getting started
+###### Prerequisites
+Make sure you already install python version 2.7 and install pip
+###### Installing
+run commands below on terminal
+```
+sudo pip install sqlparse
+sudo pip install pandas
+```
+###### Clone
+go the directory you prefer and clone this project, similar to track1
+```
+$ git clone https://github.com/JasonCHCI/queryme.git
+```
+To update
+```
+$ git checkout master
+$ git pull
+```
+To contribute:
+`follow track1`
+
---
+++ main.py
@@ -0,0 +1,48 @@
+from sqlparse import *
+from pandas import *
+from os.path import *
+import sys, getopt
+
+#def parse(arg):
+
+def main(argv):
+	inputfile = ''
+	try:
+		opts, args = getopt.getopt(argv,"hi:",["input"])
+	except getopt.GetoptError:
+		print 70*"-"
+		print "| main.py -i <inputfile> [other option] [SELECT_statement]"
+		print "| "
+		print "| Example: $ main.py -i student.csv 'select * from student'"
+		print 70*"-"
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt =='-h':
+			print 70*"-"
+			print "| main.py -i <inputfile> [other option] [SELECT_statement]"
+			print "| "
+			print "| Example: $ main.py -i student.csv 'select * from student'"
+			print 70*"-"
+			sys.exit()
+		elif opt in ("-i", "--input"):
+			inputfile = arg
+
+	return inputfile,args
+
+if __name__=="__main__":
+	file,args = main(sys.argv[1:])
+	
+	if not isfile(file):
+		print 70*"-"
+		print "| This file does not exist. Please input correct one."
+		print 70*"-"
+		sys.exit()
+	if not args:
+		print "read input file:",file,", but cannot read query statement"
+
+	else:
+		stm = args[0]
+
+
+
+
