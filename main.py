f8c0317e367c1786682842d2c425e23fe282ff23
wenhua11 <linwenhua560@gmail.com>
2017-11-14 22:02:34 -0600

    delete one command line query

--- main.py
+++ main.py
@@ -9,59 +9,22 @@ from do_query import *
 
 #from multiprocessing import Process,Manager
 
-def main(argv):
-    files=[]
-    try:
-        opts, args = getopt.getopt(argv,"i:")
-
-    except getopt.GetoptError:
-        print 90*"-"
-        print "| python main.py -i <inputfile> [SELECT_statement]"
-        print "| "
-        print '| Example: $ python main.py -i student.csv "select * from student [WHERE value=\'string\']"'
-        print '| note: string value has to be quote by single quotation mark.'
-        print 90*"-"
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-i':
-            files.append(arg)
-        else:
-            print 90*"-"
-            print "| python main.py -i <inputfile> SELECT_statement]"
-            print "| "
-            print '| Example: $ python main.py -i student.csv "select * from student [WHERE value=\'string\']"'
-            print '| note: string value has to be quote by single quotation mark.'
-            print 90*"-"
-            sys.exit(2)
-    return files,args
-
-
 
 if __name__=="__main__":
+    files=[]
+    while True:
+        print "---------------------------------------------"
+        stm = raw_input('| Input a query or input exit:\n'
+         '---------------------------------------------\n')
+        if stm=='exit':
+            sys.exit()
 
-    files,args = main(sys.argv[1:])
-    if any(not isfile(file) for file in files):
-        print 70*"-"
-        print "| The file does not exist. Please input correct one."
-        print 70*"-"
-        sys.exit()
-    if any(not file.endswith('.csv') for file in files):
-        print 70*"-"
-        print "| The file is not csv format. Please input a csv file."
-        print 70*"-"
-        sys.exit()
-    if not args:
-        print 70*"-"
-        print "| Please input a query statement."
-        print 70*"-"
-        sys.exit()
-    if len(args)>1:
-        print 70*"-"
-        print "| Please input exact one statement"
-        print 70*"-"
-        sys.exit()
-    else:
-        stm = args[0]
+        # get attributes, relations, conditions from statement
+        # attrs: attribute in SELECT clause
+        # relations: tables name in FROM clause
+        # conds: conditions in WHERE clause
+        goodstm,attrs,relations,conds = checkStatement(stm)
+
         tables ={}
         schemas ={}
         panel = {}
